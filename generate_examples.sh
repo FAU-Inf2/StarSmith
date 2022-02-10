bbc3c13302e11b2a6de7104bdf8643686a556910
Patrick Kreutzer <patrick.kreutzer@fau.de>
2022-02-10 12:42:27 +0100

    Make it easier to generate more than 10 examples per specification.

--- generate_examples.sh
+++ generate_examples.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+readonly COUNT=10
+
 out_dir="$(dirname "$0")/examples/"
 mkdir -p "$out_dir"
 
@@ -14,41 +16,41 @@ function print_step {
 }
 
 print_step "PaperSpec (without generators)"
-"$(dirname "$0")/out/paper_spec/run.sh" paper_spec --seed 0 --count 10 \
+"$(dirname "$0")/out/paper_spec/run.sh" paper_spec --seed 0 --count "$COUNT" \
   --out "$out_dir/paper_spec/program_#{SEED}.txt"
 
 print_step "PaperSpec (with generators)"
-"$(dirname "$0")/out/paper_spec/run.sh" paper_spec_gen --seed 0 --count 10 \
+"$(dirname "$0")/out/paper_spec/run.sh" paper_spec_gen --seed 0 --count "$COUNT" \
   --out "$out_dir/paper_spec_gen/program_#{SEED}.txt"
 
 print_step "C"
-"$(dirname "$0")/out/c/run.sh" c --seed 0 --count 10 \
+"$(dirname "$0")/out/c/run.sh" c --seed 0 --count "$COUNT" \
   --out "$out_dir/c/prog_#{SEED}.c"
 
 print_step "Lua"
-"$(dirname "$0")/out/lua/run.sh" lua --seed 0 --count 10 \
+"$(dirname "$0")/out/lua/run.sh" lua --seed 0 --count "$COUNT" \
   --out "$out_dir/lua/prog_#{SEED}.lua"
 
 print_step "SMT"
-"$(dirname "$0")/out/smt/run.sh" smt --seed 0 --count 10 \
+"$(dirname "$0")/out/smt/run.sh" smt --seed 0 --count "$COUNT" \
   --out "$out_dir/smt/q_#{SEED}.smt2"
 
 print_step "SMT (FP)"
-"$(dirname "$0")/out/smt/run.sh" smt_fp --seed 0 --count 10 \
+"$(dirname "$0")/out/smt/run.sh" smt_fp --seed 0 --count "$COUNT" \
   --out "$out_dir/smt_fp/q_#{SEED}.smt2"
 
 print_step "SMT (strings)"
-"$(dirname "$0")/out/smt/run.sh" smt_strings --seed 0 --count 10 \
+"$(dirname "$0")/out/smt/run.sh" smt_strings --seed 0 --count "$COUNT" \
   --out "$out_dir/smt_strings/q_#{SEED}.smt2"
 
 print_step "SMT (strings, subset)"
-"$(dirname "$0")/out/smt/run.sh" smt_strings_cvc4_1_8 --seed 0 --count 10 \
+"$(dirname "$0")/out/smt/run.sh" smt_strings_cvc4_1_8 --seed 0 --count "$COUNT" \
   --out "$out_dir/smt_strings_cvc4_1_8/q_#{SEED}.smt2"
 
 print_step "SQL (wrapper functions)"
-"$(dirname "$0")/out/sql/run.sh" sql_wrapper --seed 0 --count 10 \
+"$(dirname "$0")/out/sql/run.sh" sql_wrapper --seed 0 --count "$COUNT" \
   --out "$out_dir/sql_wrapper/query_#{SEED}.sql"
 
 print_step "SQL (arithmetic operations)"
-"$(dirname "$0")/out/sql/run.sh" sql_arith --seed 0 --count 10 \
+"$(dirname "$0")/out/sql/run.sh" sql_arith --seed 0 --count "$COUNT" \
   --out "$out_dir/sql_arith/query_#{SEED}.sql"
